22aa0f1d379a6b87f644879aeb56a915f4363bb0
nikitinskaya <nikitinskaya.v@gmail.com>
2021-01-25 19:58:11 +0300

    Add page downloading

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -6,8 +6,10 @@ import pageLoader from '../src/index.js';
 program
   .version('0.0.1', '-V, --version', 'output the version number')
   .description('Downloads web pages with resources')
+  .option('-o, --output <dirPath>', 'output dir path', process.cwd())
   .arguments('<link>')
-  .action((url) => {
-    console.log(pageLoader(url));
+  .action(async (url, { output }) => {
+    const name = await pageLoader(url, output);
+    console.log(name);
   });
 program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
+import axios from 'axios';
+import { promises as fs } from 'fs';
+import path from 'path';
+
 const getResourceName = (urlObj, ext = '.html') => {
   const linkBody = `${urlObj.hostname}${urlObj.pathname}`;
   const slug = linkBody.replace(/[^\w]/gi, '-');
   return `${slug}${ext}`;
 };
 
-const pageLoader = (link) => {
+const download = (link) => axios.get(link, { responseType: 'arraybuffer' })
+  .then(({ data }) => data.toString());
+
+const writeFile = (filepath, content) => fs
+  .writeFile(filepath, content, { encoding: 'utf-8' });
+
+const pageLoader = (link, outputDir) => {
   const urlObj = new URL(link);
-  const name = getResourceName(urlObj);
-  return name;
+  const filename = getResourceName(urlObj);
+  const filepath = path.join(outputDir, filename);
+  return download(link)
+    .then((data) => writeFile(filepath, data))
+    .then(() => filename);
 };
 
 export default pageLoader;
