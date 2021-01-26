92d391f3f97d5ee9656a712d02059f045ad14097
nikitinskaya <nikitinskaya.v@gmail.com>
2021-01-26 17:53:35 +0300

    Add image downloading

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,16 +4,19 @@
 
 import nock from 'nock';
 import {
-  test, expect, beforeAll, beforeEach,
+  describe, test, expect, beforeAll, beforeEach,
 } from '@jest/globals';
 import { promises as fs } from 'fs';
 import path from 'path';
 import os from 'os';
 import pageLoader from '../src/index.js';
 
+const base = 'https://ru.hexlet.io';
 const link = 'https://ru.hexlet.io/courses';
 const savedName = 'ru-hexlet-io-courses.html';
+const inputHTMLFile = 'ru-hexlet-io-courses.input.html';
 const outputHTMLFile = 'ru-hexlet-io-courses.output.html';
+const assetsPath = 'ru-hexlet-io-courses_files';
 
 const getFixturePath = (name) => `__tests__/__fixtures__/${name}`;
 const fileExists = async (filepath) => fs.access(filepath)
@@ -23,23 +26,57 @@ const fileExists = async (filepath) => fs.access(filepath)
 nock.disableNetConnect();
 
 let outputDir;
+let inputHTML;
 let outputHTML;
+let resources = [
+  {
+    format: 'png',
+    urlPath: '/assets/professions/nodejs.png',
+    filename: path.join(
+      assetsPath,
+      'ru-hexlet-io-assets-professions-nodejs.png',
+    ),
+  },
+];
+const formats = resources.map(({ format }) => format);
+const textFormats = [];
+const scope = nock(base).persist();
 
 beforeAll(async () => {
+  inputHTML = await fs.readFile(getFixturePath(inputHTMLFile), 'utf-8');
   outputHTML = await fs.readFile(getFixturePath(outputHTMLFile), 'utf-8');
-  nock('https://ru.hexlet.io').get('/courses').reply(200, outputHTML, { 'Access-Control-Allow-Origin': '*' });
+  scope.get('/courses').reply(200, inputHTML, { 'Access-Control-Allow-Origin': '*' });
+
+  const promises = resources.map((res) => fs.readFile(getFixturePath(res.filename))
+    .then((data) => ({ ...res, data })));
+  resources = await Promise.all(promises);
+  resources.forEach(({ urlPath, data }) => scope.get(urlPath).reply(200, data));
 });
 
 beforeEach(async () => {
   outputDir = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
 });
 
-test('Download', async () => {
-  await pageLoader(link, outputDir);
+describe('positive cases', () => {
+  test('Download page', async () => {
+    await pageLoader(link, outputDir);
 
-  const fileCreated = await fileExists(path.join(outputDir, savedName));
-  expect(fileCreated).toBe(true);
+    const fileCreated = await fileExists(path.join(outputDir, savedName));
+    expect(fileCreated).toBe(true);
 
-  const fileContent = await fs.readFile(path.join(outputDir, savedName), 'utf-8');
-  expect(fileContent).toEqual(outputHTML.trim());
+    const fileContent = await fs.readFile(path.join(outputDir, savedName), 'utf-8');
+    expect(fileContent).toEqual(outputHTML.trim());
+  });
+
+  test.each(formats)('check .%s-resource', async (format) => {
+    await pageLoader(link, outputDir);
+    const { filename, data } = resources.find((content) => content.format === format);
+    const fileWasCreated = await fileExists(path.join(outputDir, filename));
+    expect(fileWasCreated).toBe(true);
+
+    if (textFormats.includes(format)) {
+      const actualContent = await fs.readFile(path.join(outputDir, filename));
+      expect(actualContent).toEqual(data);
+    }
+  });
 });
--- src/index.js
+++ src/index.js
@@ -1,25 +1,61 @@
 import axios from 'axios';
 import { promises as fs } from 'fs';
 import path from 'path';
+import cheerio from 'cheerio';
 
-const getResourceName = (urlObj, ext = '.html') => {
-  const linkBody = `${urlObj.hostname}${urlObj.pathname}`;
+const getResourceName = (link, postfix = '.html') => {
+  const { dir, name, ext } = path.parse(link);
+  const linkBody = path.join(dir, name);
   const slug = linkBody.replace(/[^\w]/gi, '-');
-  return `${slug}${ext}`;
+  const format = ext || postfix;
+  return `${slug}${format}`;
 };
 
 const download = (link) => axios.get(link, { responseType: 'arraybuffer' })
-  .then(({ data }) => data.toString());
+  .then(({ data }) => data);
 
-const writeFile = (filepath, content) => fs
-  .writeFile(filepath, content, { encoding: 'utf-8' });
+const processAssets = (data, assetspath, link) => {
+  const $ = cheerio.load(data);
+  const tags = {
+    img: 'src',
+  };
+  const assetsLinks = Object.entries(tags).flatMap(([tag, attr]) => $(`${tag}[${attr}]`)
+    .toArray()
+    .map((el) => {
+      const elPath = $(el).attr(attr);
+      const url = new URL(elPath, link);
+      const fileLink = `${url.hostname}${url.pathname}`;
+      const relativePath = path.join(assetspath, getResourceName(fileLink, path.parse(elPath).ext));
+      $(el).attr(attr, relativePath);
+      return {
+        relativePath,
+        link: url.toString(),
+      };
+    }));
+
+  const page = $.html();
+  return { page, assetsLinks };
+};
 
 const pageLoader = (link, outputDir) => {
-  const urlObj = new URL(link);
-  const filename = getResourceName(urlObj);
+  const fileLink = new URL(link);
+  const fileLinkWithoutProto = `${fileLink.hostname}${fileLink.pathname}`;
+  const filename = getResourceName(fileLinkWithoutProto);
+  const assetsDir = getResourceName(fileLinkWithoutProto, '_files');
   const filepath = path.join(outputDir, filename);
+  const assetspath = path.join(outputDir, assetsDir);
+
   return download(link)
-    .then((data) => writeFile(filepath, data))
+    .then((data) => fs.mkdir(assetspath).then(() => data))
+    .then((data) => processAssets(data, assetsDir, link))
+    .then(({ page, assetsLinks }) => fs.writeFile(filepath, page, { encoding: 'utf-8' })
+      .then(() => assetsLinks))
+    .then((assetsLinks) => {
+      const promises = assetsLinks
+        .map(({ relativePath, link: assetLink }) => download(assetLink)
+          .then((data) => fs.writeFile(path.join(outputDir, relativePath), data)));
+      return Promise.all(promises);
+    })
     .then(() => filename);
 };
 
